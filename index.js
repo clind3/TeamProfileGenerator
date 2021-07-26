d91be5d4094bd65383927abb9837ad4e2c161a5a
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-07-26 01:43:32 -0400

    reformatted index code and created basic fcns within generateCard file

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
-//this file is going to include inquirer prompts 
-//and starting code for terminal
+//import questions, inquirer, and classes
 const inquirer = require('inquirer');
-const questions = require('./src/constants');
+const questions = require('./src/questions');
+const {Card} = require('./src/generateCard');
+const { Manager } = require('./lib/Manager');
+const {Engineer} = require('./lib/Engineer');
+const {Intern} = require('./lib/Intern');
 
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+//create question variables
+const manager = questions.generalInfo.concat(questions.managerInfo);
+const engineer = questions.generalInfo.concat(questions.engineerInfo);
+const intern = questions.generalInfo.concat(questions.internInfo);
+const card = new Card();
 
-// while(userAction != 'Quit') {
-inquirer.prompt(questions.generalInfo.concat(questions.managerInfo)).then((answers)=> {
-    //will use those answers to create Manager card
-    console.log(answers);
-    //now prompt user with options of what they want to do next
-    //will need this list to continue populating until user selects finalize
-    inquirer.prompt({
+//create user action list
+const selectAction = () => {
+    inquirer.prompt([{
         name: 'userActions',
         type: 'list',
         message: 'Select your next action:',
         choices: ['Add Engineer', 'Add Intern', 'Finalize Team'],
-    }).then((answers) => {
-        if(answers.userActions == 'Add Engineer'){
-            inquirer.prompt(questions.generalInfo.concat(questions.engineerInfo)).then((answers) => {
-                //will need to generate engineer card create engineer object
-                console.log('added engineer');
-            })
-        }else if(answers.userActions == 'Add Intern'){
-            inquirer.prompt(questions.generalInfo.concat(questions.internInfo)).then((answers) => {
-                //will need to generate engineer card create engineer object
-                console.log('added intern');
-            })
+    }]).then((action) =>{
+        switch (action.userActions) {
+            case 'Add Engineer':
+                askEngineer();
+                break;
+            case 'Add Intern':
+                askIntern();
+                break;
+            case 'Finalize Team':
+                console.log('finalizing!!');
+                break;
+                
         }
     })
+}
+
+//get engineer info
+const askEngineer = () => {
+    inquirer.prompt(engineer).then((info) => {
+    //will need to create eng. object then create card
+    let newEng = new Engineer(info.name, info.id, info.email, info.github);
+    card.getInfo(newEng);
+    console.log('added engineer');
+    selectAction();
+})}
+
+//get intern info
+const askIntern =() => {
+    inquirer.prompt(intern).then((info) => {
+        //will need to create intern object then create card
+        const newIntern = new Intern(info.name, info.id, info.email, info.school);
+        card.getInfo(newIntern);
+        console.log('added intern');
+        selectAction();
+    })
+}
+
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+inquirer.prompt(manager).then(async (answers)=>{
+    //will use those answers to create Manager card
+    const newMngr = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    //add new card with given info
+    card.getInfo(newMngr);
+    //prompt user with options of what to do next (continue to ask until user selects finalize)
+    selectAction();
 }) 
   
-// }
-
